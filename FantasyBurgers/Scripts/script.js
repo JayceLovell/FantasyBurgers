7f410aa43be0f8752feeb36074d2bd10cd5140c5
Sahilm754 <sahilm754@outlook.com>
2016-11-25 18:06:32 -0500

    Troubleshooted slideshow, and changed slideshow. Removed most of the unnecessary images. Corrected paths for different images. Added more related images.

--- FantasyBurgers/Scripts/script.js
+++ FantasyBurgers/Scripts/script.js
@@ -2,23 +2,23 @@
 /*=================================
 ||			Owl Carousel
 ==================================*/
-    $("#header-slider").owlCarousel({
+    //$("#header-slider").owlCarousel({
 
-        navigation : true, // Show next and prev buttons
-        slideSpeed : 100,
-        paginationSpeed : 400,
-        singleItem: true,
-        autoPlay: true,
-        pagination: false,
+    //    navigation : true, // Show next and prev buttons
+    //    slideSpeed : 100,
+    //    paginationSpeed : 400,
+    //    singleItem: true,
+    //    autoPlay: true,
+    //    pagination: false,
 
-        // "singleItem:true" is a shortcut for:
-        // items : 1, 
-        // itemsDesktop : false,
-        // itemsDesktopSmall : false,
-        // itemsTablet: false,
-        // itemsMobile : false
+    //    // "singleItem:true" is a shortcut for:
+    //    // items : 1, 
+    //    // itemsDesktop : false,
+    //    // itemsDesktopSmall : false,
+    //    // itemsTablet: false,
+    //    // itemsMobile : false
 
-    });
+    //});
 
 /*=================================
 ||			WOW
